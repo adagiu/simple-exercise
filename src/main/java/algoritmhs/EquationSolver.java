3994ff9869650f1e7f6523d2f23c0293980e4952
Giuseppe Adaldo <Giuseppe.Adaldo@Gamesys.co.uk>
2018-03-04 13:08:48 +0000

    new version for equation solver

--- src/main/java/algoritmhs/EquationSolver.java
+++ src/main/java/algoritmhs/EquationSolver.java
@@ -2,6 +2,40 @@ package algoritmhs;
 
 import utils.test.Test;
 
+/**
+ * Solve a given equation and return the value of x in the form of string "x=#value". The equation contains only '+', '-' operation, the variable x
+ * and its coefficient.
+ * 
+ * If there is no solution for the equation, return "No solution".
+ * 
+ * If there are infinite solutions for the equation, return "Infinite solutions".
+ * 
+ * If there is exactly one solution for the equation, we ensure that the value of x is an integer.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: "x+5-3+x=6+x-2" Output: "x=2"
+ * 
+ * Example 2:
+ * 
+ * Input: "x=x" Output: "Infinite solutions"
+ * 
+ * Example 3:
+ * 
+ * Input: "2x=x" Output: "x=0"
+ * 
+ * Example 4:
+ * 
+ * Input: "2x+3x-6x=x+2" Output: "x=-1"
+ * 
+ * Example 5:
+ * 
+ * Input: "x=x+2" Output: "No solution"
+ * 
+ * @author giuseppe.adaldo
+ *
+ */
 public class EquationSolver {
 
 	public String solve(String equation) {
@@ -16,20 +50,19 @@ public class EquationSolver {
 			if (equation.charAt(i) == '=')
 				left = false;
 
-			if (equation.charAt(i) == 'x') {
-				if (sign.equals("-"))
-					x -= getCorrectInt('1', !left);
-				else
-					x += getCorrectInt('1', !left);
-				sign = "";
+			if (isX(equation, i)) {
+				x += add(sign, "1", !left);
+			} else if (Character.isDigit(equation.charAt(i))) {
+				if (equation.length() > i + 1 && isX(equation, i + 1)) {
+					x += add(sign, "" + equation.charAt(i), !left);
+					++i;
+				} else if (equation.length() > i + 1 && Character.isDigit(equation.charAt(i + 1))) {
+					vars += add(sign, "" + equation.charAt(i) + equation.charAt(i + 1), left);
+					++i;
+				} else
+					vars += add(sign, "" + equation.charAt(i), left);
 			}
-			if (Character.isDigit(equation.charAt(i))) {
-				if (sign.equals("-"))
-					vars -= getCorrectInt(equation.charAt(i), left);
-				else
-					vars += getCorrectInt(equation.charAt(i), left);
-				sign = "";
-			}
+			sign = "";
 			if (equation.charAt(i) == '-' || equation.charAt(i) == '+')
 				sign = "" + equation.charAt(i);
 		}
@@ -40,15 +73,26 @@ public class EquationSolver {
 		return "x=" + (vars / x);
 	}
 
-	private static int getCorrectInt(char s, boolean left) {
-		return left ? -1 * Integer.valueOf("" + s) : Integer.valueOf("" + s);
+	private int add(String sign, String number, boolean left) {
+		if (sign.equals("-"))
+			return (-1) * getCorrectInt(number, left);
+		else
+			return getCorrectInt(number, left);
+	}
+
+	private boolean isX(String equation, int i) {
+		return equation.charAt(i) == 'x';
+	}
+
+	private static int getCorrectInt(String s, boolean left) {
+		return left ? -1 * Integer.valueOf(s) : Integer.valueOf(s);
 	}
 
 	public static void main(String[] args) {
 		EquationSolver solver = new EquationSolver();
 		Test.equals(solver.solve(""), "Invalid");
 		Test.equals(solver.solve(""), "Invalid");
-		Test.equals(solver.solve("   "), "Invalid");
+		Test.equals(solver.solve(" "), "Invalid");
 		Test.equals(solver.solve(null), "Invalid");
 		Test.equals(solver.solve("x+4=8-x"), "x=2");
 		Test.equals(solver.solve("-x+x+6+4=8-x"), "x=-2");
@@ -56,6 +100,7 @@ public class EquationSolver {
 		Test.equals(solver.solve("x=x"), "Infinite solutions");
 		Test.equals(solver.solve("x=x+2"), "No solution");
 		Test.equals(solver.solve("2x+3x-6x=x+2"), "x=-1");
+		Test.equals(solver.solve("3x=33+22+11"), "x=22");
 	}
 
 }
--- src/main/java/algoritmhs/LargeNumberProduct.java
+++ src/main/java/algoritmhs/LargeNumberProduct.java
@@ -1,5 +1,16 @@
 package algoritmhs;
 
+/*
+ * You have two strings representing large ints that cannot be parsed into ints. How do you find their product?
+ */
 public class LargeNumberProduct {
+
+	public String solution(String a, String b) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
