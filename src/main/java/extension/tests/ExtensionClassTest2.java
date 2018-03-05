4bd534214bfb88c9383522ef11a0c5c04b2f4640
Giuseppe Adaldo <Giuseppe.Adaldo@Gamesys.co.uk>
2018-03-05 16:24:15 +0000

    two large number mutiplication

--- src/main/java/extension/tests/ExtensionClassTest2.java
+++ src/main/java/extension/tests/ExtensionClassTest2.java
@@ -5,6 +5,7 @@ public class ExtensionClassTest2 {
 	public static void main(String[] args) {
 		Parent p = new Parent();
 		D1 d1 = new D1();
+		@SuppressWarnings("unused")
 		D2 d2 = new D2();
 		p = d1;
 		System.out.println(p);
